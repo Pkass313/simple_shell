7ac5edd1724bd8f2e7ae770f1dedb189f950776c
Pkass313 <pkass313@gmail.com>
2023-06-22 09:32:45 -0700

    2 files

--- history.c
+++ history.c
@@ -9,8 +9,8 @@
 char *get_history_file(info_t *info)
 {
 	char *buf, *dir;
-	dir = _getenv(info, "HOME=");
 
+	dir = _getenv(info, "HOME=");
 	if (!dir)
 
 		return (NULL);
@@ -70,44 +70,32 @@ int read_history(info_t *info)
 
 {
 	int m, last = 0, linecount = 0;
-	ssize_t pk, rdlen, psize = 0;
-
+	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
 	char *buf = NULL, *filename = get_history_file(info);
+
 	if (!filename)
-
 		return (0);
-
-	pk = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	free(filename);
-
-	if (pk == -1)
+	if (fd == -1)
 		return (0);
-	if (!pstat(pk, &st))
-
-		psize = st.st_size;
-
-	if (psize < 2)
-
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
 		return (0);
-
-	buf = malloc(sizeof(char) * (psize + 1));
+	buf = malloc(sizeof(char) * (fsize + 1));
 	if (!buf)
-
 		return (0);
-
-	rdlen = read(pk, buf, psize);
-	buf[psize] = 0;
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
 	if (rdlen <= 0)
-
 		return (free(buf), 0);
-	close(pk);
-
-	for (i = 0; m < psize; m++)
-
-		if (buf[i] == '\n')
+	close(fd);
+	for (m = 0; m < fsize; m++)
+		if (buf[m] == '\n')
 		{
-			buf[i] = 0;
+			buf[m] = 0;
 			build_history_list(info, buf + last, linecount++);
 			last = m + 1;
 		}
@@ -139,23 +127,25 @@ int build_history_list(info_t *info, char *buf, int linecount)
 	add_node_end(&node, buf, linecount);
 
 	if (!info->history)
-
 		info->history = node;
 
 	return (0);
 }
 
-/** renumber_history - history linked list will renumber
-* @info: Struct of potential arguments
-* Return: the new histcount
-*/
+/**
+ * renumber_history - history linked list to renumber
+ * @info: Struct of potential arguments
+ * Return: the new histcount
+ */
 
 int renumber_history(info_t *info)
 
 {
-	list_t *node = info->history;
+list_t *node = info->history;
 	int m = 0;
+
 	while (node)
+
 	{
 		node->num = m++;
 
