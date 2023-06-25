2aebed8e4ea1b261c6dc401f892edaa8af0efe1f
Gailygeemach <graciousmachuwe@gmail.com>
2023-06-24 21:54:14 -0700

    Updated shell

--- shell_loop.c
+++ shell_loop.c
@@ -124,32 +124,39 @@ void find_cmd(info_t *info)
  */
 void fork_cmd(info_t *info)
 {
-	pid_t child_pid;
+	pid_t pid = fork();
 
-	child_pid = fork();
-	if (child_pid == -1)
+	if (pid == -1)
 	{
 		perror("Error:");
 		return;
 	}
-	if (child_pid == 0)
+	else if (pid == 0)
 	{
-		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		if (execve(info->path, info->argv, get(info)) == -1)
 		{
 			free_info(info, 1);
 			if (errno == EACCES)
+			{
 				exit(126);
-			exit(1);
+			}
+			else
+			{
+				exit(1);
+			}
 		}
 	}
 	else
 	{
-		wait(&(info->status));
-		if (WIFEXITED(info->status))
+		int status;
+
+		do {
+			waitpid(pid, &status, 0);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		info->status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+		if (info->status == 126)
 		{
-			info->status = WEXITSTATUS(info->status);
-			if (info->status == 126)
-				print_error(info, "Permission denied\n");
+			print_error(info, "Permission denied\n");
 		}
 	}
 }
