286866756d3a20993a86fada4de02707dd851921
Pkass313 <128968209+Pkass313@users.noreply.github.com>
2023-06-29 22:12:22 +0200

    Update signal_handler.c

--- signal_handler.c
+++ signal_handler.c
@@ -12,31 +12,3 @@ void handle_signal(int m)
 	write(STDERR_FILENO, "\n", 1);
 	write(STDERR_FILENO, "$ ", 2);
 }
-path.c
-#include "shell.h"
-
-/**
-* find_path - path from the global enviroment locater
-* Return: NULL if path not found, otherwise PATH if path is found.
-*/
-char *find_path(void)
-{
-	int k;
-	char **env = environ, *path = NULL;
-
-	while (*env)
-	{
-		if (_strncmp(*env, "PATH=", 5) == 0)
-		{
-			path = *env;
-			while (*path && k < 5)
-			{
-				path++;
-				k++;
-			}
-			return (path);
-		}
-		env++;
-	}
-	return (NULL);
-}
