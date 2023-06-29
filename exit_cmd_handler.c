d767441f764a33232fe29829efe5dcac9be4bd9d
Pkass313 <128968209+Pkass313@users.noreply.github.com>
2023-06-29 22:09:53 +0200

    Update exit_cmd_handler.c

--- exit_cmd_handler.c
+++ exit_cmd_handler.c
@@ -13,25 +13,3 @@ void exit_cmd(char **command, char *line)
 	free_buffers(command);
 	exit(0);
 }
-#include "free_function.c"
-#include "shell.h"
-
-/**
-* free_buffers - buffer freer
-* @buf: buffer to be freed
-*
-* Return: NULL
-*/
-void free_buffers(char **buf)
-{
-	int o = 0;
-
-	if (!buf || buf == NULL)
-		return;
-	while (buf[o])
-	{
-		free(buf[o]);
-		o++;
-	}
-	free(buf);
-}
